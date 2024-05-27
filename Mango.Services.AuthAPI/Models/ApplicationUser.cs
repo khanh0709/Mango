c881c2caa1c21d0077bbcfbc89fa8c290a817808
khanh <phamduykhanhlc92@gmail.com>
2024-05-28 06:47:29 +0700

    commit auth

---
+++ Mango.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }    
+    }
+    //tao ra applicationUser extend identityUser va co them thuoc tinh. Nhung dieu nay ko lam tao them bang. Ma no se them 1 cot
+    //name cho bang IdentityUser strong DB
+}
